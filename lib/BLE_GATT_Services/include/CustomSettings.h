78b26d880a07e153b4ab3df92ce81782b1b72576
Christoph Honal <christoph.honal@web.de>
2021-07-07 02:37:56 +0200

    Implement custom settings gatt service

---
+++ lib/BLE_GATT_Services/include/CustomSettings.h
@@ -0,0 +1,82 @@
+/**
+ * @file CustomSettings.h
+ * @author Christoph Honal
+ * @brief Defines the custom settings BLE service and helpers
+ * @version 0.1
+ * @date 2021-07-06
+ */
+
+#ifndef CUSTOM_SETTINGS_SERVICE_H
+#define CUSTOM_SETTINGS_SERVICE_H
+
+#define BLE_FEATURE_GATT_SERVER 1
+#define BLE_ROLE_BROADCASTER 1
+
+#include <mbed.h>
+#include <events/mbed_events.h>
+#include "ble/BLE.h"
+
+#define UUID_CUSTOM_SETTINGS_SERVICE 0x1901 // Spec goes up to 0x184D
+#define UUID_CUSTOM_SETTINGS_CHAR    0x2C01 // Spec goes up to 0x2BC3
+
+/**
+ * @brief Provides a BLE service to read and write custom settings
+ * 
+ * @param T Type of the settings struct
+ * 
+ */
+template <typename T>
+class CustomSettingsService 
+{
+    
+    public:
+        /**
+         * @brief Construct a new Running Speed And Cadence Service object
+         * 
+         * @param ble BLE instance
+         */
+        CustomSettingsService(BLE& ble,
+            const UUID& gatt_service_uuid = UUID(UUID_CUSTOM_SETTINGS_SERVICE), 
+            const UUID& gatt_characteristic_uuid = UUID(UUID_CUSTOM_SETTINGS_CHAR));
+
+        /**
+         * @brief Set the update callback 
+         * 
+         * @param update_callback Update event callback 
+         */
+        void setCallback(const Callback<void(const T&)>& update_callback);
+
+        /**
+         * @brief Updates the settings
+         * 
+         * @param settings The data to be written
+         */
+        void updateSettings(const T& settings);
+
+        /**
+         * @brief Read the internal settings state
+         * 
+         * @param settings Target structure
+         */
+        void readSettings(T& settings);
+
+    protected:
+        BLE& _ble; //!< Reference to the BLE instance
+        Callback<void(const T&)> _update_callback = nullptr; //!< Callback for the alert
+        T _settings_value; //!< Internal settings state
+        GattCharacteristic _settingsCharacteristic; //!< settings characteristic definition
+
+        /**
+         * @brief Handles incoming data update requests from the GATT client
+         * 
+         * @param params The new settings to store
+         */
+        virtual void onDataWritten(const GattWriteCallbackParams* params);
+
+};
+
+// Define method bodies in header due to generic parameters
+#include "CustomSettings_impl.h"
+
+#endif
+ 
---
+++ lib/BLE_GATT_Services/src/CustomSettings_impl.h
@@ -0,0 +1,60 @@
+/**
+ * @file CustomSettings.cpp
+ * @author Christoph Honal
+ * @brief Implements the definitions from CustomSettings.h
+ * @version 0.1
+ * @date 2021-07-06
+ */
+
+#include "CustomSettings.h"
+#include <functional>
+
+
+template <class T>
+CustomSettingsService<T>::CustomSettingsService(BLE& ble,
+        const UUID& gatt_service_uuid, const UUID& gatt_characteristic_uuid):
+    _ble(ble),
+    _settingsCharacteristic(gatt_characteristic_uuid,
+        (uint8_t*)&_settings_value, sizeof(T), sizeof(T),
+        GattCharacteristic::BLE_GATT_CHAR_PROPERTIES_READ
+            | GattCharacteristic::BLE_GATT_CHAR_PROPERTIES_NOTIFY
+            | GattCharacteristic::BLE_GATT_CHAR_PROPERTIES_WRITE)
+{
+    // Setup BLE service definition
+    GattCharacteristic *charsTable[] = { &_settingsCharacteristic };
+    GattService CustomSettingsGATT(gatt_service_uuid, charsTable, 1);
+
+    // Attach GATT server
+    _ble.gattServer().addService(CustomSettingsGATT);
+    _ble.gattServer().onDataWritten(this, &CustomSettingsService<T>::onDataWritten);
+}
+
+template <class T>
+void CustomSettingsService<T>::setCallback(const Callback<void(const T&)>& update_callback)
+{
+    _update_callback = update_callback;
+}
+
+template <class T>
+void CustomSettingsService<T>::updateSettings(const T& settings)
+{
+    _settings_value = settings;
+    _ble.gattServer().write(_settingsCharacteristic.getValueHandle(), (uint8_t*)&_settings_value, sizeof(T));
+}
+
+template <class T>
+void CustomSettingsService<T>::readSettings(T& settings)
+{
+    settings = _settings_value;
+}
+
+template <class T>
+void CustomSettingsService<T>::onDataWritten(const GattWriteCallbackParams* params)
+{
+    if (params->handle == _settingsCharacteristic.getValueHandle()) 
+    {
+        // Callback with update value
+        memcpy(&_settings_value, params->data, min(params->len, (uint16_t)(sizeof(T))));
+        if(_update_callback != nullptr) _update_callback(_settings_value);
+    }
+}
