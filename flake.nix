5511b24bb9577f162ad184156ea82585a806006d
Christoph Honal <christoph.honal@web.de>
2022-06-22 11:57:30 +0200

    use nixos flake and direnv

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs";
+  };
+
+  outputs = { self, nixpkgs }:
+    let
+      pkgs = nixpkgs.legacyPackages.x86_64-linux;
+    in
+    {
+      devShell.x86_64-linux =
+        pkgs.mkShell {
+          shellHook = ''
+          '';
+
+          buildInputs = with pkgs; [
+            gcc-arm-embedded
+            openocd
+            platformio
+          ];
+        };
+    };
+}
